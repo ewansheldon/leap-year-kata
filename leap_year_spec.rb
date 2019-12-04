8d66ca855d3bea9e1a5a1d9b2522ec72ff56ba83
Ewan <ewan.s@cucumberwifi.io>
2019-12-04 10:57:45 +0000

    leap year divisible by 400 test

--- leap_year_spec.rb
+++ leap_year_spec.rb
@@ -8,4 +8,14 @@ describe 'leap_year?' do
     year = Year.new(1996)
     expect(year.leap_year?).to equal(true)
   end
+
+  it 'returns true if year is divisible by 400' do
+    year = Year.new(1600)
+    expect(year.leap_year?).to equal(true)
+  end
+
+  it 'returns false if year is divisible by 100, but not 400' do
+    year = Year.new(1800)
+    expect(year.leap_year?).to equal(false)
+  end
 end
