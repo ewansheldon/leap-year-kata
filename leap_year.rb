bfc11649667c55a4590a6398d525b9e3216f8467
Ewan <ewan.s@cucumberwifi.io>
2019-12-04 11:11:51 +0000

    refactor divisible checks wording

--- leap_year.rb
+++ leap_year.rb
@@ -6,7 +6,7 @@ class Year
   end
 
   def leap_year?
-    return false if !divisible_by_four || divisible_by_one_hundred && year % 400 != 0
+    return false if !divisible_by_four || divisible_by_one_hundred && !divisible_by_four_hundred
     true
   end
 
@@ -18,4 +18,7 @@ class Year
     year % 100 == 0
   end
 
+  def divisible_by_four_hundred
+    year % 400 == 0
+  end
 end
