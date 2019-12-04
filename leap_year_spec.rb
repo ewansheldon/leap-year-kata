0defe852489a63e63848c12dba0f3c2199386247
Ewan <ewan.s@cucumberwifi.io>
2019-12-04 10:56:07 +0000

    leap year divisible by four test

--- leap_year_spec.rb
+++ leap_year_spec.rb
@@ -3,4 +3,9 @@ describe 'leap_year?' do
     year = Year.new(1997)
     expect(year.leap_year?).to equal(false)
   end
+
+  it 'returns true if year is divisible by 4' do
+    year = Year.new(1996)
+    expect(year.leap_year?).to equal(true)
+  end
 end
