748600716f05003df93c002b665a6654f9b296f3
Ewan <ewan.s@cucumberwifi.io>
2019-12-04 11:05:42 +0000

    refactor guards

--- leap_year.rb
+++ leap_year.rb
@@ -4,8 +4,7 @@ class Year
   end
 
   def leap_year?
-    return false if @year % 4 != 0
-    return false if @year % 100 == 0 && @year % 400 != 0
+    return false if @year % 4 != 0 || @year % 100 == 0 && @year % 400 != 0
     true
   end
 end
