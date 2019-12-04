c4a77c9c2c42e891ae5f1d4b345bf70040e4fef8
Ewan <ewan.s@cucumberwifi.io>
2019-12-04 11:07:29 +0000

    refactor instance var

--- leap_year.rb
+++ leap_year.rb
@@ -1,10 +1,12 @@
 class Year
+  attr_reader :year
+
   def initialize(year)
     @year = year
   end
 
   def leap_year?
-    return false if @year % 4 != 0 || @year % 100 == 0 && @year % 400 != 0
+    return false if year % 4 != 0 || year % 100 == 0 && year % 400 != 0
     true
   end
 end
